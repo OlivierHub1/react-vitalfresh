e89389c521e61f9e7395abf7b241d5aa6c087d81
Olivier <olivier18102@gmail.com>
2023-07-07 15:23:54 -0400

    Css connection termine

--- src/view/connection-component/Login.tsx
+++ src/view/connection-component/Login.tsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Login.css";
 
 export const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-page">
+      <div className="form">
+        <form className="register-form">
+          <input type="text" placeholder="name" />
+          <input type="password" placeholder="password" />
+          <input type="text" placeholder="email address" />
+          <button>create</button>
+          <p className="message">
+            Already registered? <a href="#">Sign In</a>
+          </p>
+        </form>
+        <form className="login-form">
+          <input type="text" placeholder="username" />
+          <input type="password" placeholder="password" />
+          <button>login</button>
+          <p className="message">
+            Not registered? <NavLink to="/signup">Create an account</NavLink>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
--- src/view/connection-component/Signup.tsx
+++ src/view/connection-component/Signup.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Login.css";
 
 export const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="login-page">
+      <div className="form">
+        <form className="register-form">
+          <input type="text" placeholder="name" />
+          <input type="password" placeholder="password" />
+          <input type="text" placeholder="email address" />
+          <button>create</button>
+          <p className="message">
+            Already registered? <a href="#">Sign In</a>
+          </p>
+        </form>
+        <form className="login-form">
+          <input type="text" placeholder="username" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input type="password" placeholder="confirm password" />
+          <button>SIGN UP</button>
+          <p className="message">
+            Registered? <NavLink to="/login">Login</NavLink>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
